6d705769cd113d602627f6241acc7bd0720b0d31
Armin <hueneburg.armin@gmail.com>
2015-03-30 03:25:03 +0200

    forgot , in update

--- database/create.sql
+++ database/create.sql
@@ -2645,7 +2645,7 @@ CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`echo_db_usr`@`localhost` SQL SEC
 select `echo`.`patients`.`patientId` AS `patientId`,`echo`.`patients`.`doctorId` AS `doctorId`,`echo`.`patients`.`firstName` AS `firstName`,
 `echo`.`patients`.`lastName` AS `lastName`,`echo`.`patients`.`secondName` AS `secondName`,`echo`.`patients`.`socialId` AS `socialId`,
 `echo`.`patients`.`sex` AS `sex`,`echo`.`patients`.`dateOfBirth` AS `dateOfBirth`,`echo`.`patients`.`firstDiagnoseDate` AS `firstDiagnoseDate`,
-`echo`.`patients`.`fileId` AS `fileId`,`echo`.`patients`.`fullAddress` AS `fullAddress`,`echo`.`patients`.`landline` AS `landline`, `echo`.`accounts`.`enabled` AS `enabled`
+`echo`.`patients`.`fileId` AS `fileId`,`echo`.`patients`.`fullAddress` AS `fullAddress`,`echo`.`patients`.`landline` AS `landline`, `echo`.`accounts`.`enabled` AS `enabled`, 
 `echo`.`accounts`.`email` AS `email`,`echo`.`accounts`.`mobile` AS `mobile`, (SELECT `echo`.`severity`.`severity` as `severity`
         from `severity` where ((`severity`.`patientId` = `accounts`.`accountId`)) ORDER BY recordId desc LIMIT 1)    from
         (`patients`
