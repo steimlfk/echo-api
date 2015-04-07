334e582f88d856f0b58c3b8495518e8a255d753c
steimlfk <frank.steimle@ipvs.uni-stuttgart.de>
2015-04-07 10:51:57 +0200

    rewritten direct Analyzer for daily reports

--- controller/notify.js
+++ controller/notify.js
@@ -2,117 +2,193 @@
  * Created by steimlfk on 05.03.15.
  */
 var db = require('../utils.js').db;
+var service = require('../config.js').notificationService;
 var http = require('http');
 var async = require('async');
 var querystring = require('querystring');
 
-
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-// @station - an object with `freq` and `name` properties
+var postOptions = {
+    host: service.host,
+    port: '80',
+    method: 'POST',
+    headers: {
+        Authorization: service.apiKey
+    }
+};
+postOptions.headers['Content-Type'] = 'application/json';
+
 var DailyAnalyzer = function() {
 
-    // we need to store the reference of `this` to `self`, so that we can use the current context in the setTimeout (or any callback) functions
-    // using `this` in the setTimeout functions will refer to those functions, not the Radio class
-    var self = this;
-
-    // EventEmitters inherit a single event listener, see it in action
     this.on('newDailyReport', function(id) {
-        var postOptions = {
-            host: require('../config.js').notificationhost,
-            port: '80',
-            method: 'POST',
-            headers: {
-                Authorization: require('../config.js').api
+        var qry = 'SELECT a.notificationMode, a.notificationEnabled, a.accountId, a.email, a.mobile, ' +
+            'd.recordId, d.q1, d.q2, d.q3, d.q4, d.q5, d.q1a, d.q1b, d.q1c, d.q3a, d.q3b, d.q3c, ' +
+            'p.firstName, p.lastName,' +
+            'doc.notificationMode AS doc_mode, doc.notificationEnabled as doc_enabled, doc.accountId as doc_id, doc.email as doc_mail, doc.mobile AS doc_mobile ' +
+            'FROM accounts a, dailyReports d, patients p, accounts doc '+
+            'WHERE d.recordId = ? '+
+            'AND a.accountId = d.patientId AND a.accountId = p.patientId AND p.doctorId = doc.accountId;';
+        db.query(qry, id, function(err, result) {
+            var type = 0;
+            /*
+             Notification Types:
+             1 = Call your doc!
+             2 = Go to hospital!
+             3 = Your patient %name% should call you!
+             4 = Your patient %name% should go to hospital
+             */
+            var rule = 0;
+            /*
+             Rules:
+             1) Two days in a row Q1 „yes“ 				-> Notification: Call your doctor!
+             2) Q1, Q2 and Q3 answered with „yes“ 			-> Notification: Call your doctor!
+             3) Q3a or Q3b answered with „yes“ 			-> Notification: Call your doctor!
+             4) Q3c answered with yes 					-> Notification: Go to the hospital!
+             5) Two days in a row Q5 „yes“ 				-> Notification: Call your doctor!
+             6) Questions not answered for 2 or 10 days 		-> Notification: Fill in your daily Report!
+             */
+            if (result[0].q1 == 1 && result[0].q2 == 1 && result[0].q3 == 1){
+                type = 1;
+                rule = 2;
             }
-        };
-        console.log('catch');
-            db.query('SELECT * FROM accounts a inner join dailyReports d on a.accountId = d.patientId left join devices on a.accountId=devices.accountId where d.recordId = ?', id, function(err, result) {
-                var event = '';
-                if (result[0].q3a == 1 || result[0].q3b == 1 || (result[0].q1 == 1 && result[0].q2 == 1 && result[0].q3 == 1)) {
-                    event = '1';
-                }
-                if (result[0].q3c == 1) {
-                    event = '2';
-                }
-                if (result[0].notificationEnabled == 1 && event != '') {
-                    var addresses = [];
-                    switch (result[0].notificationMode) {
-                        case 'email':
-                            addresses[0] = result[0].email;
-                            postOptions.path = '/echo/email';
-                            break;
-                        case 'sms':
-                            addresses[0] = result[0].mobile;
-                            postOptions.path = '/echo/sms';
-                            break;
-                        case 'push':
-                            addresses[0] = result[0].deviceId;
-                            postOptions.path = '/echo/sns';
-                            break;
-                    }
-                    var data = JSON.stringify({
-                        'subject': 'This is an ECHO Notification',
-                        'message': 'You are dead.',
-                        'to': addresses,
-                        'label': 'ECHO',
-                        'arns': addresses,
-                        'receivers': addresses
+            if (result[0].q3a == 1 || result[0].q3b == 1) {
+                type = '1';
+                rule = 3;
+            }
+            if (result[0].q3c == 1) {
+                rule = 4;
+                type = '2';
+            }
+
+            if (type > 0){
+                var notificationqry = 'INSERT INTO notifications (accountId, date, type, subjectsAccount) VALUES (?, now(), ? ,?)';
+                var i = result[0];
+                async.parallel([
+                        function(cb) {
+                            // patients notification
+                            db.query(notificationqry, [i.accountId, type, null], cb);
+                        },
+                        function(cb) {
+                            // doctors notification
+                            db.query(notificationqry, [i.doc_id, type+2, i.accountId], cb);
+                        },
+                        function(cb){
+                            // patients notification
+                            if (service.apiKey.length > 1){
+                                var opts = {
+                                    host : postOptions.host,
+                                    port : postOptions.port,
+                                    method : postOptions.method,
+                                    headers : postOptions.headers
+                                };
+                                var msgtext = (type == 1)? 'Please call your doctor! (' + i.doc_mobile + ')' : 'Go to the Hospital!';
+                                var msg = {
+                                    'subject': 'ECHO Notification',
+                                    'message': msgtext
+                                };
+
+                                switch (i.notificationMode) {
+                                    case 'email':
+                                        msg.to[0] = i.email;
+                                        opts.path = '/echo/email';
+                                        break;
+                                    case 'sms':
+                                        msg.reveivers[0] = i.mobile;
+                                        msg.label ='ECHO';
+                                        opts.path = '/echo/sms';
+                                        break;
+                                    case 'push':
+                                        msg[0].arns = '';
+                                        opts.path = '/echo/sns';
+                                        break;
+                                }
+                                var payload = JSON.stringify(msg);
+                                opts.headers['Content-Length'] = payload.length;
+
+
+                                var request = http.request(opts, function (res) {
+                                    res.setEncoding('utf8');
+                                    res.on('data', function (data) {
+                                        console.log(data);
+                                    })
+                                });
+                                request.write(data);
+                                request.end();
+                                cb('patients notification was sent');
+                            }
+                            else cb ('omitting sending patients notification!')
+                        },
+                        function(cb){
+                            // doctors notification
+                            if (service.apiKey.length > 1){
+
+                                var opts = {
+                                    host : postOptions.host,
+                                    port : postOptions.port,
+                                    method : postOptions.method,
+                                    headers : postOptions.headers
+                                };
+                                var patient_name = i.firstName + ' ' + i.lastName;
+                                var msgtext = (type == 1)? 'Your patient ' + patient_name + ' should call you!' : 'Your patient ' + patient_name + ' should go to the hospital';
+                                var msg = {
+                                    'subject': 'ECHO Notification',
+                                    'message': msgtext
+                                };
+
+                                switch (i.notificationMode) {
+                                    case 'email':
+                                        msg.to[0] = i.doc_email;
+                                        opts.path = '/echo/email';
+                                        break;
+                                    case 'sms':
+                                        msg.reveivers[0] = i.doc_mobile;
+                                        msg.label ='ECHO';
+                                        opts.path = '/echo/sms';
+                                        break;
+                                    case 'push':
+                                        msg[0].arns = '';
+                                        opts.path = '/echo/sns';
+                                        break;
+                                }
+                                var payload = JSON.stringify(msg);
+                                opts.headers['Content-Length'] = payload.length;
+
+
+                                var request = http.request(opts, function (res) {
+                                    res.setEncoding('utf8');
+                                    res.on('data', function (data) {
+                                        console.log(data);
+                                    })
+                                });
+                                request.write(data);
+                                request.end();
+                                cb('doctors notification was sent');
+                            }
+                            else cb ('omitting sending patients notification!')
+                        }
+                    ],
+                    function (err, res){
+                        if (err) {
+                            console.log('ERROR while sending or storing notification!')
+                            console.log(err);
+                        }
                     });
-
-                    switch (event) {
-                        case '1':
-                            var data = JSON.stringify({
-                                'subject': 'This is an ECHO Notification',
-                                'message': 'Call your doctor.',
-                                'to': addresses,
-                                'label': 'ECHO',
-                                'arns': addresses,
-                                'receivers': addresses
-                            });
-                            break;
-
-                        case '2':
-                            var data = JSON.stringify({
-                                'subject': 'This is an ECHO Notification',
-                                'message': 'Go to the hospital.',
-                                'to': addresses,
-                                'label': 'ECHO',
-                                'arns': addresses,
-                                'receivers': addresses
-                            });
-                            break;
-                    }
-
-                    postOptions.headers['Content-Length'] = data.length;
-                    postOptions.headers['Content-Type'] = 'application/json';
-
-
-                    var request = http.request(postOptions, function (res) {
-                        res.setEncoding('utf8');
-                        res.on('data', function (data) {
-                            console.log(data);
-                        })
-                    });
-
-                    request.write(data);
-                    request.end();
-                    console.log('E Mail was sent');
-                }
-            });
-
-
+            }
+        });
     });
+
 
     // For sending notifications to persons who have answered the question q1 with yes two days in a row.
     this.on('twoDayAnalyzes', function(id) {
+        if (service.apiKey.length < 2) return;
         var postOptions = {
-            host: require('../config.js').notificationhost,
+            host: service.host,
             port: '80',
             method: 'POST',
             headers: {
-                Authorization: require('../config.js').api
+                Authorization: service.apiKey
             }
         };
         db.query('SELECT a.accountId, notificationEnabled, email, mobile, deviceId from accounts a ' +
@@ -176,11 +252,11 @@ var DailyAnalyzer = function() {
 
     this.on('inactiveAnalyzes', function() {
         var postOptions = {
-            host: require('../config.js').notificationhost,
+            host: service.host,
             port: '80',
             method: 'POST',
             headers: {
-                Authorization: require('../config.js').api
+                Authorization: service.apiKey
             }
         };
         db.getConnection(function(err, connection) {
@@ -253,12 +329,13 @@ var DailyAnalyzer = function() {
     });
 
     this.on('goldAnalyzes', function(id) {
+        return;
         var postOptions = {
-            host: require('../config.js').notificationhost,
+            host: service.host,
             port: '80',
             method: 'POST',
             headers: {
-                Authorization: require('../config.js').api
+                Authorization: service.apiKey
             }
         };
         db.query('SELECT goldAnalyzes(?) as new, notificationMode, email, mobile, deviceId from accounts left join devices on accounts.accountId=devices.accountId where accounts.accountId=?;',
