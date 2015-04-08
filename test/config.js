962e511c887c935e3132a41e945dcbd505a37e13
steimlfk <frank.steimle@ipvs.uni-stuttgart.de>
2015-04-08 10:43:51 +0200

    Added vars for testing

--- test/config.js
+++ test/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     url : 'http://localhost:3000',
     admin_username : 'nimda',
-    admin_pwd : 'nimda'
+    admin_pwd : 'nimda',
+    analysis_doc_mail : '',
+    analysis_pat_mail : ''
 };
